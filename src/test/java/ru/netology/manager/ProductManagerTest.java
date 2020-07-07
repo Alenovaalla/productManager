d9c666dcb25572f6c5a37490a0ba524df8e272cf
Alla Alenova <alla_alenova@mail.ru>
2020-07-07 23:27:30 +0300

    Changed test

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -3,32 +3,29 @@ package ru.netology.manager;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductManagerTest {
      ProductRepository repository= new ProductRepository();
-     Product first = new Product(1, "A", 1);
-    Product second = new Product(2, "B", 2);
-    Product third = new Product(3, "C", 2);
+     Product first = new Book(1, "A", 5, "Толстой");
+    Product second = new Book(2, "B", 2, "Пушкин");
+    Product third = new Smartphone(3, "B", 4, "Sony");
     Product[] products = new Product[]{first, second, third};
 
     @Test
 
-    public void shouldSearchBookById () {
-        ProductManager manager= new ProductManager(repository);
+    public void shouldSearchByText () {
+        ProductManager manager = new ProductManager(repository);
         repository.save(first);
         repository.save(second);
         repository.save(third);
-      String text = "1";
-        Product[] expected = new Product[]{third};
-        Product[] actual = manager.searchBy("3");
+        Product[] expected = new Product[]{first};
+        Product[] actual = manager.searchBy("A");
 
-        assertArrayEquals(expected,actual);
-
-
-
+        assertArrayEquals(expected, actual);
     }
 
 }
